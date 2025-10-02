c8ced73ee9e7717d4c32add7f9caa26d0f5c049c
SimonMousen <simonuwusierra@gmail.com>
2025-10-01 20:40:57 -0500

    Implementacion de crud de workoutHorario

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,24 +4,14 @@ const usersRoutes = require('./users.routes');
 const exercisesRoutes = require('./exercises.routes');
 const workoutPlanRoutes = require('./workoutPlan.routes');
 const workoutExerciseRoutes = require('./workoutExercise.routes');
-
-
-
-
-
-
+const workoutHorarioRoutes = require('./workoutHorario.routes');
 
 
 router.use('/users', usersRoutes);
 router.use('/exercises', exercisesRoutes);
 router.use('/workoutplan', workoutPlanRoutes);
 router.use('/workoutexercise', workoutExerciseRoutes);
-
-
-
-
-
-
+router.use('/workouthorario', workoutHorarioRoutes);
 
 
 module.exports = router;
