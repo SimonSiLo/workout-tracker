e2765414e97bda8442fb454c4a6cd18d3ae1dcb0
SimonMousen <simonuwusierra@gmail.com>
2025-10-01 19:56:42 -0500

    implementacion de crud de workoutExercise

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersRoutes = require('./users.routes');
 const exercisesRoutes = require('./exercises.routes');
-const workoutPlanRoutes = require('./workoutPlan.routes')
-
+const workoutPlanRoutes = require('./workoutPlan.routes');
+const workoutExerciseRoutes = require('./workoutExercise.routes');
 
 
 
@@ -15,7 +15,7 @@ const workoutPlanRoutes = require('./workoutPlan.routes')
 router.use('/users', usersRoutes);
 router.use('/exercises', exercisesRoutes);
 router.use('/workoutplan', workoutPlanRoutes);
-
+router.use('/workoutexercise', workoutExerciseRoutes);
 
 
 
